4598205cf05193cb67199daa761120decca810d0
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-01-30 15:36:23 +0100

    LifecyclerObservers included to observe mainactivity

--- app/src/main/java/com/aloine/kotlin_lifecycleobserver/MainActivity.kt
+++ app/src/main/java/com/aloine/kotlin_lifecycleobserver/MainActivity.kt
@@ -2,11 +2,42 @@ package com.aloine.kotlin_lifecycleobserver
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG,"Owner onCreate")
+    lifecycle.addObserver(MainActivityObserver())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"Owner onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "Owner onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"Owner onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"Owner onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "Owner onDestroy")
+    }
+    companion object {
+        private val TAG : String = MainActivity::class.java.simpleName
     }
 }
